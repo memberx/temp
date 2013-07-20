a69c83b702ec47612db6c6c1b24844c443d17a07
memberx <akhvlediani.giorgi@gmail.com>
2013-07-21 00:01:50 +0400

    test

--- index.php
+++ index.php
@@ -1,11 +1,26 @@
-<?php
+<html>
+    <head>
+        <script type="text/javascript" src="jquery.min.js"></script>
+    </head>
+    <body>
+        <script>
+            
+            $(document).ready(function(){
 
-
-try{
-	echo 'ssss';
-}catch (Exception $e)
-{
-	echo 'sdsdsds';
-}
-
-?>
+                $("#go_btn").click(function(){
+                    console.log(document.location.hash);
+                });
+            });
+        </script>
+        
+        <?php
+        
+            /*echo '<pre>';
+            print_r($_SERVER);
+            */
+        ?>
+        
+        
+        <span id="go_btn">Go</span>
+    </body>
+</html>
